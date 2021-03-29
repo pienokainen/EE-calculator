2b72ebb843a43703ee9cfda3f2b6c22c210a21ff
pienokainen <77006105+pienokainen@users.noreply.github.com>
2021-03-29 10:48:05 +0300

    calculator build almost ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,145 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'EECalculator',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'EE-Calculator',
+              style: TextStyle(fontSize: 25.0),
+            ),
+          ),
+        ),
+        body: ButtonsBuild(),
       ),
-      home: MyHomePage(title: 'Calculator by EE-Team'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ButtonsBuild extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ButtonsBuildState createState() => _ButtonsBuildState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+
+class _ButtonsBuildState extends State<ButtonsBuild> {
+  Widget buildButton(String buttonText) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(35.0),
+        ),
+        child: Text(
+          '$buttonText',
+          style: TextStyle(
+              fontSize: 30.0,
+              fontStyle: FontStyle.normal,
+              color: Colors.grey[900]),
+        ),
+        onPressed: () {
+          print('Number pressed');
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(10.0),
         ),
-      ),
+        Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            '0',
+                            style: TextStyle(fontSize: 45.0),
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          buildButton('7'),
+                          buildButton('8'),
+                          buildButton('9'),
+                          ElevatedButton(
+                            child: Text(
+                              '+',
+                              style: TextStyle(
+                                  fontSize: 30.0,
+                                  fontStyle: FontStyle.normal,
+                                  color: Colors.grey[900]),
+                            ),
+                            onPressed: () {
+                              print('+ sign pressed');
+                            },
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          buildButton('4'),
+                          buildButton('5'),
+                          buildButton('6'),
+                          ElevatedButton(
+                            child: Text(
+                              '-',
+                              style: TextStyle(
+                                  fontSize: 30.0,
+                                  fontStyle: FontStyle.normal,
+                                  color: Colors.grey[900]),
+                            ),
+                            onPressed: () {
+                              print('- sign pressed');
+                            },
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          buildButton('1'),
+                          buildButton('2'),
+                          buildButton('3'),
+                          ElevatedButton(
+                            child: Text(
+                              '=',
+                              style: TextStyle(
+                                  fontSize: 30.0,
+                                  fontStyle: FontStyle.normal,
+                                  color: Colors.grey[900]),
+                            ),
+                            onPressed: () {
+                              print('= sign pressed');
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        )
+      ],
     );
   }
 }
